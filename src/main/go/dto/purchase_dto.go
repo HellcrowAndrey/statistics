a082e453f904b0d01082b4d775d39738e1dd750b
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-08-02 15:28:02 +0300

    update purchase_dto.go

--- src/main/go/dto/purchase_dto.go
+++ src/main/go/dto/purchase_dto.go
@@ -9,6 +9,6 @@ type PurchaseDto struct {
 	Id        uint          `json:"id" example:"1"`
 	CreatedAt time.Time     `json:"createAt" example:"2019-11-09T21:21:46+00:00"`
 	OrderId   uint          `json:"orderId" example:"1"`
-	AccountId uint          `json:"accountId omitempty" example:"1"`
+	AccountId uint          `json:"accountId,omitempty" example:"1"`
 	Products  []*ProductDto `json:"products"`
 }
